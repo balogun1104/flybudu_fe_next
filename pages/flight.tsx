5fea35c1cd83a5656ba40ade8dbe73c67ed45785
Balogun <razak2free@gmail.com>
2024-04-23 13:00:00 +0100

    Flight Fix

--- pages/flight.tsx
+++ pages/flight.tsx
@@ -3,53 +3,61 @@ import Navbar from "../components/Navbar/indexBlack";
 import styles from "../styles/flight.module.css";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import FlightChunk from "../components/Flight/FlightChunk";
-import arrow from "@/public/assets/images/double chevron.png"
-import Footer from "../components/Footer/index"
-import FilterImg from "@/public/assets/images/filter (2).png"
-import Edit from "@/public/assets/images/Edit.png"
+import arrow from "@/public/assets/images/double chevron.png";
+import Footer from "../components/Footer/index";
+import FilterImg from "@/public/assets/images/filter (2).png";
+import Edit from "@/public/assets/images/Edit.png";
 import MobileNav from "../components/MobileNavBar";
-import FlightFilter from "../components/FlightFilter/index"
+import FlightFilter from "../components/FlightFilter/index";
 import Image from "next/image";
-import { useAppSelector } from '@/redux/hooks';
+import { useAppSelector } from "@/redux/hooks";
 // import SliderControl from "../components/SliderControl/SliderControl";
-import  Link  from 'next/link';
-
+import Link from "next/link";
 
 const Flight = () => {
-  const { searchCriteria, flightData, loading, error } = useAppSelector((state) => state.flight);
+  const { searchCriteria, flightData, loading, error } = useAppSelector(
+    (state) => state.flight
+  );
 
-  // console.log(flightData, "................................")
+  console.log(flightData, "................................");
   const [visible, setVisible] = useState(false);
-  const toggleDivs = () =>{
-      setVisible(!visible)
-  }
-  const [openEdit, setOpenEdit] = useState(false)
+  const toggleDivs = () => {
+    setVisible(!visible);
+  };
+  const [openEdit, setOpenEdit] = useState(false);
+
+  const firstFlight = flightData[0];
+  const otherFlights = flightData.slice(1);
 
   return (
     <div className={styles.flightContainer}>
-      
       <div className={styles.fixed}>
-      <Navbar />
-      
+        <Navbar />
       </div>
       <div className={styles.Navbar}>
-         <Link href="/"><IoIosArrowBack style={{color:"white" , marginLeft:"30px", fontSize:"25px"}}/></Link>
-         <div className={styles.navbarText}>
-            <span className={styles.location}>Lagos - Abuja</span>
-            <span>Feb. 29, 2024 - Mar. 14, 2024, 1 Pass, Economy</span>
-         </div>
-         <div className={styles.imgDiv}> 
-        <Link href="/mobileflight"> <Image src={Edit} alt=""/></Link>
-        <Link href="/filter"> <Image src={FilterImg}   alt=""/></Link>
-         </div>
-        
+        <Link href="/">
+          <IoIosArrowBack
+            style={{ color: "white", marginLeft: "30px", fontSize: "25px" }}
+          />
+        </Link>
+        <div className={styles.navbarText}>
+          <span className={styles.location}>Lagos - Abuja</span>
+          <span>Feb. 29, 2024 - Mar. 14, 2024, 1 Pass, Economy</span>
+        </div>
+        <div className={styles.imgDiv}>
+          <Link href="/mobileflight">
+            {" "}
+            <Image src={Edit} alt="" />
+          </Link>
+          <Link href="/filter">
+            {" "}
+            <Image src={FilterImg} alt="" />
+          </Link>
+        </div>
       </div>
-    
-      {
-           openEdit && <FlightFilter onClick={() => setOpenEdit(false)} />
-        }
-    
-      
+
+      {openEdit && <FlightFilter onClick={() => setOpenEdit(false)} />}
+
       <div className={styles.flightWrapper}>
         <div style={{ margin: "30px" }}>
           {" "}
@@ -57,63 +65,71 @@ const Flight = () => {
             We Found 56 Flights From Lagos To Abuja
           </span>
         </div>
-        
+
         <div className={styles.flightContent}>
           <div className={styles.flightContentOne}>
-          <FlightFilter onClick={() => setOpenEdit(false)} />
+            <FlightFilter onClick={() => setOpenEdit(false)} />
           </div>
           <div className={styles.flightContentTwo}>
-
-          <div className={styles.editDiv}>
-  <span className={styles.edit}  onClick={toggleDivs}>Edit</span>
-  </div>
-            <div className={styles.dateDiv}   style={{display: visible ? "flex" : "none"}}>
+            <div className={styles.editDiv}>
+              <span className={styles.edit} onClick={toggleDivs}>
+                Edit
+              </span>
+            </div>
+            <div
+              className={styles.dateDiv}
+              style={{ display: visible ? "flex" : "none" }}
+            >
               <IoIosArrowBack />
-             <div className={styles.opor}>
-             <div className={styles.flexDiv}>
-                <span>Tue, May 14</span>
-                <span className={styles.blueText}>#160,000</span>
+              <div className={styles.opor}>
+                <div className={styles.flexDiv}>
+                  <span>Tue, May 14</span>
+                  <span className={styles.blueText}>#160,000</span>
+                </div>
+                <div className={styles.flexDiv}>
+                  <span>Tue, May 14</span>
+                  <span className={styles.blueText}>#160,000</span>
+                </div>
+                <div className={`${styles.flexDiv} ${styles.display}`}>
+                  <span>Tue, May 14</span>
+                  <span className={`${styles.blueText} ${styles.blue}`}>
+                    #160,000
+                  </span>
+                </div>
+                <div className={styles.flexDiv} style={{ borderLeft: "none" }}>
+                  <span>Tue, May 14</span>
+                  <span className={styles.blueText}>#160,000</span>
+                </div>
+                <div className={styles.flexDiv}>
+                  <span>Tue, May 14</span>
+                  <span className={styles.blueText}>#160,000</span>
+                </div>
+                <div className={`${styles.flexDiv} ${styles.flex}`}>
+                  <span>Tue, May 14</span>
+                  <span className={styles.blueText}>#160,000</span>
+                </div>
               </div>
-              <div className={styles.flexDiv}>
-                <span>Tue, May 14</span>
-                <span className={styles.blueText}>#160,000</span>
-              </div><div className={`${styles.flexDiv} ${styles.display}`}>
-                <span>Tue, May 14</span>
-                <span className={`${styles.blueText} ${styles.blue}`}>#160,000</span>
-              </div><div className={styles.flexDiv} style={{borderLeft:"none"}}>
-                <span>Tue, May 14</span>
-                <span className={styles.blueText}>#160,000</span>
-              </div><div className={styles.flexDiv}>
-                <span>Tue, May 14</span>
-                <span className={styles.blueText}>#160,000</span>
-              </div><div className={`${styles.flexDiv} ${styles.flex}`}>
-                <span>Tue, May 14</span>
-                <span className={styles.blueText}>#160,000</span>
-              </div>
-             </div>
               <IoIosArrowForward />
             </div>
-            <FlightChunk />
+            {firstFlight && <FlightChunk flightData={firstFlight} />}
 
             <h4 className={styles.other}> Other Flights Options</h4>
-            <FlightChunk />
+            {otherFlights.map((flight, index) => (
+              <FlightChunk key={index} flightData={flight} />
+            ))}
 
-            <FlightChunk />
             <div className={styles.loadDiv}>
               {" "}
               <p>Load More Result</p> <Image alt="" src={arrow} />
             </div>
           </div>
         </div>
-
       </div>
-      
-      <MobileNav/>
-      
+
+      <MobileNav />
+
       <Footer />
-      
     </div>
-    
   );
 };
 
